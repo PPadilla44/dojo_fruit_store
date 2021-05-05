52aae3f860361a21464aeba7eb1ec15844c6e037
Pablo Padilla <Padilla.Pablo1998@gmail.com>
2021-05-05 14:26:55 -0700

    initial commit

--- server.py
+++ server.py
@@ -7,8 +7,15 @@ def index():
 
 @app.route('/checkout', methods=['POST'])         
 def checkout():
-    print(request.form)
-    return render_template("checkout.html")
+    fname_form = request.form['first_name']
+    lname_form = request.form['last_name']
+    id_form = request.form['student_id']
+    strawberry_form = request.form['strawberry']
+    raspberry_form = request.form['raspberry']
+    apple_form = request.form['apple']
+    total_fruits = int(strawberry_form) + int(raspberry_form) + int(apple_form)
+    print(f"Charging {fname_form} {lname_form} for {total_fruits} fruits")
+    return render_template("checkout.html", strawberry_form = strawberry_form, fname_form=fname_form,lname_form=lname_form,id_form=id_form,raspberry_form=raspberry_form,apple_form=apple_form)
 
 @app.route('/fruits')         
 def fruits():
